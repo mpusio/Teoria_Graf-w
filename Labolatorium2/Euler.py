c88ea32de4140572f59eff3bbfe624a3031ee3dd
mpusio <32813834+mpusio@users.noreply.github.com>
2018-04-17 18:46:04 +0200

    Zmiana koncepcji
    Zmiana koncepcji z oddzielnych funkcji na jedną klasę całkowitą, gdzie niektóre funkcji się na siebie nakładają, Wystarczy podać samą listę sąsiadów, nie trzeba zwracać kolejno stopni wierzchołków itp ...

--- Labolatorium2/Euler.py
+++ Labolatorium2/Euler.py
@@ -1,96 +1,92 @@
-#Sprawdz czy graf jest Eulerowski lub poleulerowski
+class Graph:
 
-def DFS(list_neighbors, l_edg):
-    """Przeszukuje graf w glab, sprawdza czy jest spojny"""
-    list_vertices = list(set(list_neighbors))
-    actuall_vertices = None
-    stack = []
-    vertices_visited = []
+    def __init__(self, list_neighbors):
+        self.list_neighbors = list_neighbors
 
-    def neighbors_v(v):
-        """"Zwraca liste sasiadow danego wierzcholka"""
-        neigbors_v = []
-        for pairs in l_edg:
-            if pairs[0] == v:
-                neigbors_v.append(pairs[1])
-            elif pairs[1] == v:
-                neigbors_v.append(pairs[0])
-        return neigbors_v
+    def DFS(self):
+        """Przeszukuje graf w glab, sprawdza czy jest spojny"""
 
+        def list_edges():
+            """Zwraca nowa liste z parami wierzcholkow (krawedziami grafu): [(v1,v2),(v1,v3),...]"""
+            list_neighbors = list(self.list_neighbors)
+            list_edg = zip(list_neighbors[::2], list_neighbors[1::2])
+            return list_edg
 
-    def neighbors_substract_visited():
-        "Odejmuje od sasiadow odwiedzone wierzcholki"
-        for nums in vertices_visited:
-            if nums in neighbors:
-                neighbors.remove(nums)
+        def neighbors_v(v):
+            """"Zwraca liste sasiadow danego wierzcholka"""
+            neigbors_v = []
+            for pairs in l_edg:
+                if pairs[0] == v:
+                    neigbors_v.append(pairs[1])
+                elif pairs[1] == v:
+                    neigbors_v.append(pairs[0])
+            return neigbors_v
 
+        def neighbors_substract_visited():
+            "Odejmuje od sasiadow odwiedzone wierzcholki"
+            for nums in vertices_visited:
+                if nums in neighbors:
+                    neighbors.remove(nums)
 
-    actuall_vertices = list_vertices[0]
-    vertices_visited.append(actuall_vertices)
-    stack.append(actuall_vertices)
+        l_edg = list_edges()
+        actuall_vertices = None
+        stack = []
+        vertices_visited = []
 
-    while stack != []:
-        stack[-1] = actuall_vertices
-        neighbors = neighbors_v(actuall_vertices)
-        neighbors_substract_visited()
+        vertices_visited.append(actuall_vertices)
+        stack.append(actuall_vertices)
 
-        if actuall_vertices in neighbors:
-            vertices_visited.append(actuall_vertices)
-            stack.append(actuall_vertices)
+        while stack != []:
+            stack[-1] = actuall_vertices
+            neighbors = neighbors_v(actuall_vertices)
+            neighbors_substract_visited()
+
+            if actuall_vertices in neighbors:
+                vertices_visited.append(actuall_vertices)
+                stack.append(actuall_vertices)
+            else:
+                stack.pop()
+                
+        return True  # zwraca true jezeli jest spojny
+
+    def amount_vertices(self):
+        """Zwraca liczbe wierzcholkow w grafie"""
+        return len(set(self.list_neighbors))
+
+    def degrees_vertices(self):
+        """Zwraca liste kolejnych stopni wierzcholkow w grafie"""
+        amount_ver = self.amount_vertices()
+        list_neighbors = list(self.list_neighbors)
+        list_degrees = [list_neighbors.count(items) for items in
+                        range(amount_ver)]  # zlicza wierzcholki w liscie i dodaje do nowej listy
+        return list_degrees
+
+    def check_Euler(self):
+        degrees = list(self.degrees_vertices())
+        even = []
+        for values in degrees:
+            if values % 2 == 0:
+                even.append(values)
+
+        if (len(even) == self.amount_vertices()) and self.DFS():
+            print "Graf jest eulerowski"
         else:
-            stack.pop()
+            print "Graf nie jest eulerowski"
 
-    return True #zwraca true jezeli jest spojny
+    def check_half_Euler(self):
+        odd = []
+        degrees = list(self.degrees_vertices())
+        for values in degrees:
+            if values % 2 != 0:
+                odd.append(values)
 
+        if len(odd) <= 2 and self.DFS():
+            print "Graf jest poleulerowski"
+        else:
+            print "Graf nie jest poleulerowski"
 
-def list_edges(list_neighbors):
-    """Zwraca nowa liste z parami wierzcholkow (krawedziami grafu): [(v1,v2),(v1,v3),...]"""
-    list_neighbors = list(list_neighbors)
-    list_edg = zip(list_neighbors[::2], list_neighbors[1::2])
-    return list_edg
-
-
-def amount_vertices(list_neighbors):
-    """Zwraca liczbe wierzcholkow w grafie"""
-    return len(set(list_neighbors))
-
-
-def degrees_vertices(list_neighbors, amount_ver):
-    """Zwraca liste kolejnych stopni wierzcholkow w grafie"""
-    list_neighbors = list(list_neighbors)
-    list_degrees = [list_neighbors.count(items) for items in range(amount_ver)] #zlicza wierzcholki w liscie i dodaje do nowej listy
-    return list_degrees
-
-def check_Euler(degrees):
-    degrees = list(degrees)
-    for values in degrees:
-        if values % 2 != 0:
-            return False
-    return True
-
-def check_half_Euler(degrees):
-    odd =[]
-    degrees = list(degrees)
-    for values in degrees:
-        if values % 2 != 0:
-            odd.append(values)
-
-    if len(odd) > 2:
-        return False
-    else:
-        return True
-
-
+#Przykladowe wywolanie:
 list_neighbors = [0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 1, 2, 1, 4, 1, 5, 2, 3, 2, 4, 3, 5, 4, 5] # graf poleulerowski z przykladu
-
-ver_amount = amount_vertices(list_neighbors)
-degs_v = degrees_vertices(list_neighbors, ver_amount)
-l_edg = list_edges(list_neighbors)
-first_condition = DFS(list_neighbors, l_edg)
-
-if check_Euler(degs_v) and  DFS(list_neighbors, l_edg):
-    print "Graf jest eulerowski"
-elif check_half_Euler(degs_v) and DFS(list_neighbors, l_edg):
-    print "Graf jest poleulerowski"
-else:
-    print "Graf nie jest eulerowski jak rowniez nie jest poleulerowski"
+graf = Graph(list_neighbors)
+graf.check_Euler()
+graf.check_half_Euler()
